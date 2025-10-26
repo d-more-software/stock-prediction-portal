8c7c39d34ffb7a7316f658de37e9ed69f7565b0e
d-more-software <md.moredavid@protonmail.com>
2025-10-26 12:35:54 +0300

    homePage react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+        <footer className="footer py-3 my-3" >
+        <hr className="border-bottom" />
+        <p className="text-light text-center" >© 2025 David More  </p>
+        </footer>
+    </>
+  )
+}
+export default Footer
