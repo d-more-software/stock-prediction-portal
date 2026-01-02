0d02c5c3875a1d82f3ae182232367138e7d3eeab
d-more-software <md.moredavid@protonmail.com>
2026-01-02 17:12:18 +0300

    add render start script and deployment config

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -134,6 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173'
 ]
+CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com"]
+
 
 
 REST_FRAMEWORK = {
