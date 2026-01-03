f2e1014e064b98bb5375e9ec90883a358aa7e336
d-more-software <md.moredavid@protonmail.com>
2026-01-03 11:26:27 +0300

    setup-postgres-database-render

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,11 +87,14 @@ WSGI_APPLICATION = 'stock_prediction_main.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+        "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
+
+
+
 
 
 # Password validation
